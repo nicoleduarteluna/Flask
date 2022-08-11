a32e4b0a61e91f7abe9f9b77e4f2881caaa73656
Nicole Duarte Luna <nicole.luna@totvs.com.br>
2022-08-10 22:08:29 -0300

    Introdução Jinja2, render_template

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("projeto")
 
 @app.route ("/")
 def ola_mundo ():
-    return "Olá mundo! Esse é meu primeiro projeto Flask!!", 200
+    # return "Olá mundo! Esse é meu primeiro projeto Flask!!", 200
+    nome = "Nicole Duarte Luna"
+    produtos = [
+        {
+            "nome": "Ração",
+            "preco": "15,00"
+        },
+        {
+            "nome": "Playstation",
+            "preco": "4.999,99"
+        }
+    ]
+    return render_template("alo.html", n = nome, aProdutos = produtos), 200
 
 app.run()
